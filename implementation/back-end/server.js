18d884d46eca387c1c8a8aadd25c00d24547211b
Yusuf Habib Alfatha <y.habib.alfatha@gmail.com>
2023-08-19 06:03:29 -0700

    Add database.js

--- implementation/back-end/server.js
+++ implementation/back-end/server.js
@@ -1,5 +1,7 @@
 // Package
 const express = require('express')
+// Module
+const db = require('./database/database')
 // Init
 const app = express()
 // ==>
@@ -9,4 +11,7 @@ app.get('/', (req, res) => {
 app.listen(4000, () => {
     console.log('haii')
     console.log('the hole')
+    db.connect(() => {
+        console.log('db connected')
+    })
 })
